947b98922564d23e04613a2584d5d9b13ceb702a
Subaru-Arakawa <subaru@takepartsfactory.com>
2019-04-05 22:12:07 +0900

    Initial Commit

---
+++ getDemoTH/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  getDemoTH
+//
+//  Created by Subaru Arakawa on 2019/04/05.
+//  Copyright © 2019 1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
